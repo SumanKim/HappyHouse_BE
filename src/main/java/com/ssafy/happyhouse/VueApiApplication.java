61b4cb179090c79f390e875a74a4711b281051f6
Suman Kim <kimsm9822@gmail.com>
2022-05-19 17:11:34 +0900

    first commit

--- src/main/java/com/ssafy/happyhouse/VueApiApplication.java
+++ src/main/java/com/ssafy/happyhouse/VueApiApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VueApiApplication {
+public class VueApiApplication { 
      
 	public static void main(String[] args) {
 		SpringApplication.run(VueApiApplication.class, args);
