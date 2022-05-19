c297830aa753e0de4dda7e051faadf0d39594bfa
tlgus31 <tlgus31@gmail.com>
2022-05-19 17:37:16 +0900

    master commit

---
+++ src/main/java/com/ssafy/happyhouse/service/BoardService.java
@@ -0,0 +1,15 @@
+package com.ssafy.happyhouse.service;
+
+import java.util.List;
+
+import com.ssafy.happyhouse.dto.Board;
+
+public interface BoardService {
+	public List<Board> retrieveBoard();
+	public List<Board> getBoardList(String text);
+	public Board detailBoard(int articleno);
+	public void updateHit(int articleno);
+	public boolean writeBoard(Board board);
+	public boolean updateBoard(Board board);
+	public boolean deleteBoard(int articleno);
+}
