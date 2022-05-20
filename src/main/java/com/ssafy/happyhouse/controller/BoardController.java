18c84261d02677e3ac48184fd998b1370cfafd84
tlgus31 <tlgus31@gmail.com>
2022-05-20 17:05:08 +0900

    add comment dto,mapper,service,controller

--- src/main/java/com/ssafy/happyhouse/controller/BoardController.java
+++ src/main/java/com/ssafy/happyhouse/controller/BoardController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,7 +26,7 @@ import io.swagger.annotations.ApiOperation;
 @Api("BoardController V1")
 @RestController
 @RequestMapping("/board")
-//@CrossOrigin(origins = { "*" }, maxAge = 6000)
+@CrossOrigin(origins = { "*" }, maxAge = 6000)
 public class BoardController {
 
 	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
